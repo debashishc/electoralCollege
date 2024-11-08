f7211d4e81579b8f370754b2e0919551fb6d4a59
debashishc <u4610248@anu.edu.au>
2024-11-08 22:56:26 +1100

    feat: improved exception descriptions

--- exceptions.py
+++ exceptions.py
@@ -1,15 +1,30 @@
 class ElectoralCollegeError(Exception):
-    """Base exception"""
+    """Base exception class for all Electoral College related errors."""
     pass
+
 
 class InvalidStateError(ElectoralCollegeError):
-    """Raised for an invalid state"""
+    """
+    Raised when an invalid state is provided or when state data is incorrect.
+    Examples: Invalid state code, missing state data, etc.
+    """
     pass
+
 
 class InvalidPartyError(ElectoralCollegeError):
-    """Raised for an invalid party"""
+    """
+    Raised when an invalid party is provided or when party data is incorrect.
+    Examples: Invalid party code, inconsistent party data, etc.
+    """
     pass
 
+
 class InvalidVoteCountError(ElectoralCollegeError):
-    """Raised for invalid vote counts, checked for total votes"""
+    """
+    Raised when vote counts are invalid.
+    Examples: 
+    - Total electoral votes ≠ 538
+    - Negative vote counts
+    - Vote counts exceed state maximum
+    """
     pass
